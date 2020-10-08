7fbe8e597322cd31aec26c785c03f3e4b753b81b
Oleg Mozzhechkov <oleg.mozzhechkov@gmail.com>
2020-10-08 12:11:39 +0300

    #51 - Added TokenFormat and OAuthTokenFormat

---
+++ src/test/java/com/artipie/http/client/auth/OAuthTokenFormatTest.java
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Artipie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.http.client.auth;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link OAuthTokenFormat}.
+ *
+ * @since 0.5
+ */
+class OAuthTokenFormatTest {
+
+    @Test
+    void shouldReadToken() {
+        MatcherAssert.assertThat(
+            new OAuthTokenFormat().token(
+                String.join(
+                    "\n",
+                    "{",
+                    "\"access_token\":\"mF_9.B5f-4.1JqM\",",
+                    "\"token_type\":\"Bearer\",",
+                    "\"expires_in\":3600,",
+                    "\"refresh_token\":\"tGzv3JOkF0XG5Qx2TlKWIA\"",
+                    "}"
+                ).getBytes()
+            ),
+            new IsEqual<>("mF_9.B5f-4.1JqM")
+        );
+    }
+}
